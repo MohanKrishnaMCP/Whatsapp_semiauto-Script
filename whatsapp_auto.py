90eb4fb0b130d84761057be3575878716fa6c1d9
pirate_sama <mcpmk1998@gmail.com>
2019-11-19 16:23:00 +0530

    version 1

---
+++ whatsapp_auto.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome(executable_path = 'D:/Programs/chrome driver/chromedriver.exe')
+driver.get('https://web.whatsapp.com/')
+
+print("---------------Whatsapp semiauto message-----------------")
+
+name = []
+no_users = int(input('Enter the number of receivers: '))
+
+for i in range(no_users):
+	u_name = str(input('Enter the name: '))
+	name.append(u_name)
+
+print(name)
+msg = input('Enter the message: ')
+count = int(input('Enter the count: '))
+
+input('Enter anything after scanning')
+
+for receiver in name:
+	user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(receiver))
+	user.click()
+
+	msg_box = driver.find_element_by_class_name('_3FeAD')
+
+	for i in range(count):
+		msg_box.send_keys(msg)
+		button = driver.find_element_by_class_name('_3M-N-')
+		button.click()
